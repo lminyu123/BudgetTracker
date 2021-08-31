ac4bf51799010f679050ae13d6fcd17d93674d1f
lminyu123 <61035433+lminyu123@users.noreply.github.com>
2021-08-31 10:13:18 -0700

    Initial commit

---
+++ Minyu.Infrastructure.BudgetTracker/Data/BudgetTrackerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minyu.ApplicationCore.BudgetTracker.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minyu.Infrastructure.BudgetTracker.Data
+{
+   public class BudgetTrackerDbContext:DbContext
+    {
+        public BudgetTrackerDbContext(DbContextOptions<BudgetTrackerDbContext> options) : base(options)
+        {
+            
+        }
+        //dbset create table
+        public DbSet<User> Users { get; set; }
+        //public DbSet<Income>Incomes { get; set; }
+        //public DbSet<Expenditure> Expenditures { get; set; }
+    }
+}
---
+++ Minyu.Infrastructure.BudgetTracker/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Minyu.ApplicationCore.BudgetTracker.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minyu.Infrastructure.BudgetTracker.Services
+{
+   public class UserService: IUserService
+    {
+
+    }
+}
